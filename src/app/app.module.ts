c89cfc3de02e71c321d67561fa59f1ac4b3350fc
Aayush Choudhry <choudhryaayush@gmail.com>
2017-04-17 21:32:00 +0530

    learned routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,28 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { HighlightDirective } from './highlight.directive';
+import { UsersComponent } from './users/users.component';
+import { Routes ,RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes= [
+  {path:'', component: HomeComponent},
+  {path: 'users', component: UsersComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HighlightDirective,
+    UsersComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
